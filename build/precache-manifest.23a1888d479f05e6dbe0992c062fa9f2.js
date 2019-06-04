d28a6038d2f96fffaa06b2c3a72d05e58401dc9f
Lisihonghong <806196726@qq.com>
2019-06-04 22:07:12 +0800

    second commit

---
+++ build/precache-manifest.23a1888d479f05e6dbe0992c062fa9f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672753f4a154bb3f889a18d6c2098078",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a2d28797788f0947eeac",
+    "url": "./static/css/2.e121dfff.chunk.css"
+  },
+  {
+    "revision": "de2aae6849c5e0caf946",
+    "url": "./static/css/main.e23c6661.chunk.css"
+  },
+  {
+    "revision": "a2d28797788f0947eeac",
+    "url": "./static/js/2.77e52be3.chunk.js"
+  },
+  {
+    "revision": "de2aae6849c5e0caf946",
+    "url": "./static/js/main.732b7ee1.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  }
+]);
